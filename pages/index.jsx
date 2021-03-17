317434f70b0cbde5b89e184ab42b70655a6168a8
coffmanjrp <coffmanjrp@gmail.com>
2021-03-16 18:18:36 -0700

    Recreate home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,5 +2,12 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <>Hello World</>;
+  return (
+    <div className="page-container">
+      <div className={styles.main}>
+        <h1>Next News App</h1>
+        <h3>Your one stop shop for the latest news articles</h3>
+      </div>
+    </div>
+  );
 }
